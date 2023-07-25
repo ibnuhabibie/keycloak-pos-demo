46079273179f1189bbc83487441f1fdc4e9eec15
Ibnu Habibie <m.habibieibnu@gmail.com>
2023-07-25 23:28:01 +0700

    buold

--- src/App.vue
+++ src/App.vue
@@ -61,16 +61,24 @@ watch(keycloak, (newVal, oldVal) => {
         <th class="p-4 border">Email</th>
         <td class="p-4 border">{{ encodedToken.email }}</td>
       </tr>
+      <tr>
+        <th class="p-4 border">Role</th>
+        <td class="p-4 border">
+          <span v-for="role in encodedToken.resource_access.pos.roles" class="bg-gray-100 p-2 rounded">
+            {{ role }}
+          </span>
+        </td>
+      </tr>
     </table>
     <RouterView />
     <div class="absolute right-5 top-5">
       <a :href="logoutUrl" class="px-4 py-2 bg-red-600 text-white rounded">Logout</a>
     </div>
     <div class="absolute bottom-0 left-auto bg-gray-300 flex gap-3 w-1/2 justify-center rounded h-10">
-      <a class="bg-gray-100 px-3 rounded mt-[-50px] h-20 flex items-center" href="#">
+      <a class="bg-gray-100 px-3 rounded mt-[-50px] h-20 flex items-center" href="https://keycloak-pos.on-dev.info">
         <img src="https://www.freeiconspng.com/uploads/point-of-sale-icon-7.png" width="50">
       </a>
-      <a class="bg-gray-100 px-3 rounded mt-[-50px] h-20 flex items-center" href="#">
+      <a class="bg-gray-100 px-3 rounded mt-[-50px] h-20 flex items-center" href="https://keycloak-crm.on-dev.info">
         <img
           src="https://e7.pngegg.com/pngimages/116/111/png-clipart-ab-brothers-pvt-ltd-company-salesforce-com-computer-icons-crm-icon-company-text.png"
           width="50">
